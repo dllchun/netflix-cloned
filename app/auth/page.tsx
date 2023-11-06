63195402525a7c7700a04e5220e251f7395309bc
dllchun <wingchuncheung0108@gmail.com>
2023-11-07 00:03:03 +0800

    day 4 next js

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,16 +1,78 @@
+"use client";
+
 import Input from "@/components/Input";
+import Link from "next/link";
+import { useState, useCallback } from "react";
 
 const AuthPage = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+ 
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
-      <div className="bg-black w-full h-full bg-opacity-50">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5 z-50 ">
           <img src="/images/logo.png" alt="logo" className="h-12 " />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 max-w-md rounded-md w-full ">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-            <div className="flex flex-col gap-4"></div>
+          <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:w-2/5 max-w-md rounded-md w-full ">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Create an account"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="username"
+                  onChange={(ev: any) => setUsername(ev.target.value)}
+                  value={username}
+                  label="Username"
+                  type="text"
+                />
+              )}
+
+              <Input
+                id="email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                value={email}
+                label="Email"
+                type="email"
+              />
+              <Input
+                id="password"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                value={password}
+                label="Password"
+                type="text"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-red-600 h-11 text-white rounded-md mt-8 w-full mb-8 hover:bg-red-700 transition cursor-pointer"
+            >
+              {variant === "login" ? "Login" : "Sign Up"}
+            </button>
+            <div className="flex">
+              <p className="text-neutral-500 font-semibold">
+                {variant === "login"
+                  ? "First time using Netflix?"
+                  : "Already have a an account?"}
+
+                <span
+                  onClick={toggleVariant}
+                  className="text-white cursor-pointer "
+                >
+                  {variant === "login" ? "Create an account" : "Login in"}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,61 @@
-const Input = () => {
-  return <Input />;
+interface props {
+  id: string;
+  onChange?: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input = (props: props) => {
+  return (
+    <div className="relative">
+      <input
+        onChange={props.onChange}
+        value={props.value}
+        id={props.id}
+        type={props.type}
+        className="
+        block
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+        text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+        invalid:border-b-1
+        "
+      />
+
+      <label
+        className="
+    absolute
+    text-md
+    text-zinc-400
+    duration-150
+    transform 
+    -translate-y-3
+    scale-75
+    top-4
+    z-10
+    origin-[0]
+    left-6
+    peer-placeholder-shown:scale-100
+    peer-placeholder-shown:translate-y-0
+    peer-focus:scale-75
+    peer-focus:-translate-y-3
+    "
+        htmlFor={props.id}
+      >
+        {props.label}
+      </label>
+    </div>
+  );
 };
 
 export default Input;
